bd3e6790018aa35979b14ee01a67f349696a3a62
KonstantinMasich <KonstantinMasich@outlook.com>
2017-04-03 13:56:36 -0700

    autocommit

--- PYTHON-exercises/Intro to Python/Ex5/Ex5_q3.py
+++ PYTHON-exercises/Intro to Python/Ex5/Ex5_q3.py
@@ -9,16 +9,30 @@ import os
 
 # Change dir constant if needed.
 # By default, "scores" directory with json files should be in the
-# same directory as this file (Ex5_q3.py)
+# SAME directory as this file (Ex5_q3.py)
 WORKING_DIR = "scores"
 
 for _, _, filenames in os.walk(WORKING_DIR):
     for fname in filenames:
         try:
+            # Loading data from file:
             f = open(WORKING_DIR + "/" + fname)
-            print("File: " + WORKING_DIR + "/" + fname)
+            print(WORKING_DIR + "/" + fname)
             data = json.load(f)
+            # Retrieving data:
+            mathgrades = []
+            scigrades  = []
+            litgrades  = []
             for row in data:
-                print(row['math'])
+                mathgrades.append(row['math'])
+                scigrades.append(row['science'])
+                litgrades.append(row['literature'])
+            # Printing results:
+            print("\tscience: min", min(scigrades), "max", max(scigrades), \
+                  "average", (sum(scigrades)/len(scigrades)))
+            print("\tliterature: min", min(litgrades), "max", max(litgrades), \
+                  "average", (sum(litgrades)/len(litgrades)))
+            print("\tmath: min", min(mathgrades), "max", max(mathgrades), \
+                  "average", (sum(mathgrades)/len(mathgrades)))
         except IOError:
             print("Error opening file!")
